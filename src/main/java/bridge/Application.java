87f79df9d3c809dd941786a8307025eaff37a386
5jeong <wjdgns4385@naver.com>
2023-12-12 21:52:01 +0900

    [Feat] 게임실행기능 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,12 @@
 package bridge;
 
+import bridge.controller.BridgeGameController;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        BridgeNumberGenerator generator = new BridgeRandomNumberGenerator();
+        BridgeGameController bridgeGameController = new BridgeGameController(generator);
+        bridgeGameController.gameStart();
     }
 }
